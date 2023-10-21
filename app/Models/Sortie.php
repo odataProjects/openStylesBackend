002443b9c3f6c4f57f486418289fda63a91c6f28
Cyrilos <mampisandryfaniriniaikoc@gmail.com>
2023-10-21 08:38:09 +0200

    fixing error in Output class model

--- app/Models/Sortie.php
+++ app/Models/Sortie.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Entree extends Model
+class Sortie extends Model
 {
     protected $table = "sortie";
     protected $primaryKey = "code_sortie";
